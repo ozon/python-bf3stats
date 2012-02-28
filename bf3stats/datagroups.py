4c7fc8cc37bde5a32ac061279dd38990a5f6c48b
Harry Gabriel <h.gabriel@nodefab.de>
2012-02-28 18:30:00 +0100

    change class objDict to allow access data as dict or objects

--- bf3stats/datagroups.py
+++ bf3stats/datagroups.py
@@ -3,12 +3,22 @@ from bf3stats.utils import _to_str
 
 
 class objDict(object):
-    def __init__(self, d):
-        for k, v in d.iteritems():
+    '''The recursive class for building and representing objects with.'''
+    # http://stackoverflow.com/questions/1305532/convert-python-dict-to-object
+
+    def __init__(self, obj):
+        for k, v in obj.iteritems():
             if isinstance(v, dict):
                 setattr(self, _to_str(k).title(), objDict(v))
             else:
-                setattr(self, _to_str(k), v)
+                setattr(self, k, v)
+
+    def __getitem__(self, val):
+        return self.__dict__[val]
+
+    def __repr__(self):
+        return '{%s}' % str(', '.join('%s : %s' % (k, repr(v)) for (k, v) in self.__dict__.iteritems()))
+
 
 class DataGroup(object):
 
