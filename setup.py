6f304e7443303cfef5d8bc7d9fe7c05b74a53ee3
Harry Gabriel <h.gabriel@nodefab.de>
2012-02-29 02:25:37 +0100

    add setup.py - close #1

---
+++ setup.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="bf3stats",
+      version="0.99",
+      description="Python interface to the bf3stats.com API",
+      author="Harry Gabriel",
+      author_email="h.gabriel@nodefab.de",
+      url="https://github.com/ozon/python-bf3stats",
+      license="GPLv2",
+      packages=find_packages(),
+      keywords="bf3stats library"
+     )
